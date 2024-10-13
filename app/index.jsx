ae1e8135f678d9389a4d10b78c681cd97bc2a234
Samran Ahmed <samranahmed03@gmail.com>
2024-10-13 23:09:00 +0500

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
+
+
+
+export default HomeScreen
+
