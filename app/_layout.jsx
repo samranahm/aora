538e1347074d9dc7301997a440b0dc0cc6a16e17
Samran Ahmed <samranahmed03@gmail.com>
2024-10-14 23:25:48 +0500

    feat: add new screens, assets, and constants

---
+++ app/_layout.jsx
@@ -0,0 +1,44 @@
+
+import { useFonts } from 'expo-font';
+import { SplashScreen, Slot, Stack } from 'expo-router';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+ const RootLayout = () => {
+const [fontLoaded, error] = useFonts({
+  "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+  "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+  "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+  "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+  "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+  "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+  "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+  "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+  "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  
+});
+
+useEffect (() => {
+  if (error) throw error;
+
+  if (fontLoaded) SplashScreen.hideAsync();
+}, [fontLoaded, error])
+
+if (!fontLoaded && !error) return null;
+
+  return (
+    
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        
+      </Stack>
+   
+  );
+}
+
+export default RootLayout;
