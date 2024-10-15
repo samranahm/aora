7cb62e83acd37c741d9dada82e2c2c94f4eb7f9f
Samran Ahmed <samranahmed03@gmail.com>
2024-10-15 22:57:00 +0500

    feat: Update auth layout and index, add CustomButt on component
    - Modified _layout.jsx in the auth folder to enhance layout structure.
    - Updated index.jsx to integrate new features and improve UI.
    - Added CustomButton.jsx to provide reusable button functionality.

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
-import React from 'react'
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, Text, View, Image } from "react-native";
+import { Link } from "expo-router";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "../components/CustomButton";
+
+import { images } from "../constants";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl font-pextrabold">Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{color:'blue'}}>Go to home</Link>
-    </View>
-  )
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%'}}>
+        <View className="w-full justify-center 
+        items-center min-h-[85vh] px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="w-[380px] h-[300px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-2xl text-white font-bold text-center">
+              Discover Endless Possibilities with{' '}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[115px] h-[13px] absolute
+              -bottom-1 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular 
+          text-gray-100 mt-7 text-center">Where creativity meets innovation:
+            emabark on the journey of limitless exploration
+          </Text>
+
+          <CustomButton
+           title="Continue with Email"
+           handlePress={() => {}}
+           containerStyle="w-full mt-7"
+           />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" 
+      style="light" />
+    </SafeAreaView>
+  );
 }
-
-
